e156d1497dbe62b5de1824911e98036c4db404e2
Mustafa-haj-hamoud <142098665+Mustafa-haj-hamoud@users.noreply.github.com>
2024-04-17 23:27:54 +0300

    solve question 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
 
+const isAlpha = function(ch){
+    return /^[A-Za-z0-9]$/.test(ch);
+}
+
+const palindromes = function (str) {
+    if (str.length === 1) return true;
+
+    let left = 0;
+    let right = str.length - 1;
+
+    while (left <= right){
+        while (!isAlpha(str[left])) left++;
+        while (!isAlpha(str[right])) right--;
+        let leftChar = str[left].toLowerCase()
+        let rightChar = str[right].toLowerCase()
+        if (leftChar !== rightChar) return false;
+        left++;
+        right--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
