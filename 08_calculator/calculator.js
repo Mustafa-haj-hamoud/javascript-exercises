33b838fc8bd943be712e69251aaac7e5f624ee58
Mustafa-haj-hamoud <142098665+Mustafa-haj-hamoud@users.noreply.github.com>
2024-04-17 23:08:17 +0300

    solve exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce( (sum, current) => (current+sum), 0 );
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce( (result, current) => (result * current));
 };
 
-const power = function() {
-	
+const power = function(base, pwr) {
+  if (pow = 0) return 1;
+  let result = base;
+
+  for (let i=1; i<pwr; i++) {
+    result = result * base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  if (num === 1) return 1;
+  let result = 1;
+  for (let i = 1; i<=num; i++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
